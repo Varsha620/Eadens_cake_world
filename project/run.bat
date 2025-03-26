7ed1d9769104b302c300836dc70eba921cc3af36
Varsha_Sabu <stu.vs.pro@gmail.com>
2025-03-26 14:02:33 +0530

    First state

---
+++ project/run.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Users\soora\AppData\Local\Programs\Python\Python313\python.exe" -m pip install -r requirements.txt
+"C:\Users\soora\AppData\Local\Programs\Python\Python313\python.exe" app.py
